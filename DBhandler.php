2aa38888ab8f28bbd81f08255505244979a5f399
Sing <singfong91@gmail.com>
2023-08-29 11:20:26 +0200

    test

--- DBhandler.php
+++ DBhandler.php
@@ -5,5 +5,37 @@ class DbHandler
     private $userName = "root";
     private $password = "";
 
+    public function getUser($naam, $password)
+    {
+        try {
+            $pdo = new PDO($this->dataSource, $this->userName, $this->password);
+            $statement = $pdo->prepare("SELECT * FROM gebruikers WHERE Naam = :naam AND Wachtwoord = :password");
+            $statement->bindParam(":naam", $naam, PDO::PARAM_STR);
+            $statement->bindParam(":password", $password, PDO::PARAM_STR);
+            $statement->execute();
+
+            $user1 = $statement->fetch(PDO::FETCH_ASSOC);
+            return $user1;
+        } catch (PDOException $e) {
+            var_dump($e);
+            return null;
+        }
+    }
+
+    public function createUser($username, $password)
+    {
+        try {
+            $pdo = new PDO($this->dataSource, $this->userName, $this->password);
+            $statement = $pdo->prepare("INSERT INTO gebruikers (username, password) VALUES (:naam, :password)");
+            $statement->bindParam(":username", $naam, PDO::PARAM_STR);
+            $statement->bindParam(":password", $password, PDO::PARAM_STR);
+            $statement->execute();
+            $id = $pdo->lastInsertId();
     
+            return $id;
+        } catch (PDOException $e) {
+            var_dump($e);
+            return false;
+        }
+    }
 }
