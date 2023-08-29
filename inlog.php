1289176977fd8331491461e7a5079f301bc3359e
giulia <murariu.giulia@gmail.com>
2023-08-29 10:40:55 +0200

    Start

---
+++ inlog.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/inlog.css">
+    <title>Inloggen</title>
+</head>
+<body>
+    <nav>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="event.php">Event</a></li>
+        <li><a href="login.php">Inloggen</a></li>
+      </ul>
+    </nav>    
+</body>
+</html>
