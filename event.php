ce420ff17a2a6fb125b3c50d85368e8759327523
EinarasVil <ps212258@edu.summacollege.nl>
2023-08-29 11:52:12 +0200

    started on event.php

--- event.php
+++ event.php
@@ -4,9 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/event.css">
-    <title>Event</title>
+    <title>Events</title>
 </head>
 <body>
+    <div class="grid-container">
+        <?php
+        // Include the database connection file
+        require_once 'DBHandler.php';
+
+        try {
+            // Perform database operations using $pdo
+
+            // Example query
+            $statement = $pdo->query("SELECT * FROM events");
+            $statement = $pdo->query("SELECT * FROM joinedevents");
+
+            $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+            // Output the results
+            foreach ($rows as $row) {
+                $eventid = $row['eventID'];
+                $name = $row['eventName'];
+                $desc = $row['eventDesc'];
+                $location = $row['eventLocation'];
+                
+                $counter = $row['count(userid)'];
+
+                echo "<div class='event-block'>
+                        <h3>$name</h3>
+                        <p>$desc</p>
+                        <p>$location</p>
+                        <p>$counter<p>
+                    </div>";
+            }
+        } catch (PDOException $e) {
+            die("Query failed: " . $e->getMessage());
+        }
+
+        ?>
     
 </body>
 </html>
