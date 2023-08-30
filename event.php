ae1ebe3b063ba5cc26d3d5c3ba612205e0864ce9
EinarasVil <ps212258@edu.summacollege.nl>
2023-08-30 11:18:43 +0200

    code added for deleting and counting events

--- event.php
+++ event.php
@@ -13,10 +13,37 @@
 </head>
 
 <body>
-<h1>Summa Tinder</h1>
+    <?php
+        if(isset($_POST['JoinEvent']))
+        {
+            require_once 'DBhandler.php';
+        $dbHandler = new dbHandler();
+            if(isset($_POST['user']) && isset($_POST['event']))
+            {
+                $dbHandler->InsertJoin123($_POST['user'], $_POST['event']);
+            }
+            else{
+                echo "error";
+            }
+        }
+        if(isset($_POST['LeaveEvent']))
+        {
+            require_once 'DBhandler.php';
+        $dbHandler = new dbHandler();
+        if(isset($_POST['user']) && isset($_POST['event']))
+            {
+                $dbHandler->DeleteJoin($_POST['user'], $_POST['event']);
+            }
+            else{
+                echo "error2";
+            }
+
+        }
+    ?>
+    <h1>Summa Tinder</h1>
     <nav>
         <ul>
-            <li><a href="index.php">Home</a></li>
+            <li><a href="index.php">Home </a></li>
             <li><a href="event.php">Events</a></li>
             <li><a href="inlog.php">Inloggen</a></li>
         </ul>
@@ -31,8 +58,10 @@
             // Perform database operations using $pdo
 
             // Example query
-            $countrows = $dbHandler->selectAllJoinedEvents();
+            $rows = $dbHandler->SelectAllWithJoinedCount();
             $rows = $dbHandler->SelectAllEVents();
+
+            $userid = 2;
 
             // Output the results
             foreach ($rows as $row) {
@@ -40,12 +69,26 @@
                 $name = $row['eventName'];
                 $desc = $row['eventDesc'];
                 $location = $row['eventLocation'];
+                //$rows = count($dbHandler->selectAllJoinedEvents());
+                //$count = $row['joinedcount'];
+                $count1 = $dbHandler->CountEventNumber($eventid);
+                $count = $count1[0]['COUNT(userID)'];
 
                 echo "<div class='event-block'>
                         <h3>$name</h3>
                         <p>$desc</p>
                         <p>$location</p>
-                        <p><p>";
+                        <p class=cntpple>$count people are joining this event.</p>
+                        <form action='event.php' method='POST'>
+                        <input type='hidden' value='$userid' name='user'>
+                        <input type='hidden' value='$eventid' name='event'>
+                        <input type='submit' name='JoinEvent'>
+                        <input type='submit' name='LeaveEvent'>
+                        </form>";
+                        
+                        
+                         
+                    
                 echo "</div>";
             }
         } catch (PDOException $e) {
