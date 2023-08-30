b4408e7b4e06932f8e15c534c92802d5c1820447
EinarasVil <ps212258@edu.summacollege.nl>
2023-08-30 11:27:28 +0200

    merging event.php and event functions in dbhandler

--- DBhandler.php
+++ DBhandler.php
@@ -155,15 +155,6 @@ class DbHandler
             var_dump($exception);
             return false;
         }
-        if($count ==0 && count != null){
-            InsertJoin123($userid, $eventid);
-        }
-        else if($count >0 && count != null){
-            deleteJoin($userid, $eventid);
-        }
-        else{
-            return false;
-        }
         
     }
 
@@ -186,7 +177,7 @@ class DbHandler
     public function deleteJoin($userid, $eventid)
     {
         try{
-            $pdo = new PDO($this->dataSource, $this->username, $this->password);
+            $pdo = new PDO($this->dataSource, $this->userName, $this->password);
             $statement = $pdo->prepare("DELETE FROM `joinedevents` WHERE userID = :UserID and eventID = :EventID;");
             $statement->bindParam("UserID", $userid, PDO::PARAM_INT);
             $statement->bindParam("EventID", $eventid, PDO::PARAM_INT);
