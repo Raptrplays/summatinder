ad1d5fc89ac63c69f5368b73542d3bf776f78ccf
EinarasVil <ps212258@edu.summacollege.nl>
2023-08-31 11:47:23 +0200

    events and userid fixed with sessions

--- event.php
+++ event.php
@@ -10,10 +10,13 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/event.css">
+    <script src="./popup.js" defer></script>
 </head>
 
 <body>
     <?php
+    session_start();
+    $existence= 0;
         if(isset($_POST['JoinEvent']))
         {
             require_once 'DBhandler.php';
@@ -39,6 +42,33 @@
             }
 
         }
+        if(isset($_POST['DeleteEvent']))
+        {
+            require_once 'DBhandler.php';
+        $dbHandler = new dbHandler();
+        if(isset($_POST['user']) && isset($_POST['event']))
+            {
+                $dbHandler->deleteJoinAll($_POST['event']);
+                $dbHandler->DeleteEvent($_POST['user'], $_POST['event']);
+            }
+            else{
+                echo "error2";
+            }
+        }
+        if(isset($_POST['CreateEvent']))
+        {
+            require_once 'DBhandler.php';
+        $dbHandler = new dbHandler();
+        if(isset($_POST['eventName']) && isset($_POST['eventDesc']) && isset($_POST['eventLocation']) && isset($_POST['user']))
+            {
+                //InsertEvent($eventname, $eventdesc, $eventlocation, $userid)
+                $dbHandler->InsertEvent($_POST['eventName'], $_POST['eventDesc'], $_POST['eventLocation'], $_POST['user']);
+            }
+            else{
+                echo "error2";
+            }
+        }
+        
     ?>
     <h1>Summa Tinder</h1>
     <nav>
@@ -60,8 +90,24 @@
             // Example query
             $rows = $dbHandler->SelectAllWithJoinedCount();
             $rows = $dbHandler->SelectAllEVents();
+            
 
-            $userid = 4;
+            if(isset($_SESSION['GebruikersId']))
+            {
+            $userid = $_SESSION['GebruikersId'];
+            }
+            else{
+                $userid = 0;
+            }
+            /*
+            if(isset($_SESSION['$GebruikersId']))
+            {
+                $userid = $_SESSION['GebruikersId'];
+            }
+            else{
+                $userid =0;
+            }*/
+            
             $data = array();
             
             // Output the results
@@ -75,8 +121,9 @@
                 $count1 = $dbHandler->CountEventNumber($eventid);
                 $count = $count1[0]['COUNT(userID)'];
                 $joined = $dbHandler->CheckJoined($userid, $eventid);
-
+                $created = $dbHandler->CheckCreatedEvent($userid, $eventid);
                 $item;
+                $delete;
 
                 if($joined == true)
                 {
@@ -86,21 +133,30 @@
                 {
                   $item = false;
                 }
-                //foreach( )
-                {
-
-                }
-                // $data = array(
-                //     array('text' => 'Item 1', 'isRed' => true),
-                //     array('text' => 'Item 2', 'isRed' => false),
-                // );
-
-                // foreach ($data as $item) {
-                    // Determine the CSS class based on the 'isRed' boolean value
+                
                     $cssClass = $item ? 'green-eventblock' : 'gray-eventblock';
-                // }
 
-                echo "<div class='$cssClass'>
+                    if($created == true)
+                {
+                  $delete = true;
+                }
+                else
+                {
+                  $delete = false;
+                }
+                if($userid == 0)
+                {
+                    echo "<div class='$cssClass'>
+                        <h3>$name</h3>
+                        <p>$desc</p>
+                        <p>$location</p>
+                        <p class=cntpple>$count people are joining this event.</p>";
+                        echo "</div>";
+                }
+                else{
+                    $DeleteClass = $delete ? 'vis-deletebtn' : 'invis-deletebtn';
+
+                    echo "<div class='$cssClass'>
                         <h3>$name</h3>
                         <p>$desc</p>
                         <p>$location</p>
@@ -110,19 +166,45 @@
                         <input type='hidden' value='$eventid' name='event'>
                         <input class='joinbtn' type='submit' value='Join' name='JoinEvent'>
                         <input class='leavebtn' type='submit' value='Leave' name='LeaveEvent'>
+                        <input class='$DeleteClass' type='submit' value='Delete' name='DeleteEvent'>
                         </form>";
                         
                         
                          
                     
                 echo "</div>";
+                }
+                
+                    
             }
             
         } catch (PDOException $e) {
             die("Query failed: " . $e->getMessage());
         }
+        
 
         ?>
+        <button class="button-forum" onclick="openPopup()">Toevoegen</button>
+
+<div id="popup" class="popup">
+    <div class="popup-content">
+        <span class="close-btn">&times;</span>
+        <form method="post" action='event.php'>
+            <label for="eventName">Naam van evenement:</label>
+            <input type="text" id="eventName" name="eventName" required><br><br>
+
+            <label for="eventDesc">Event description:</label>
+            <input type="text" id="eventDesc" name="eventDesc" required><br><br>
+
+            <label for="eventLocation">Evenement locatie:</label>
+            <input type="text" id="eventLocation" name="eventLocation" required><br><br>
+
+            <input type='hidden' value='$userid' name='user'>
+
+            <input class="button-forum" type="submit" value="Create" name='CreateEvent'>
+        </form>
+    </div>
+</div>
     
 
 </body>
